2f4969361b620d8b6f8340e4f99a2e8473e3143d
vintx64 <vintx64@gmail.com>
2023-04-07 06:46:10 +0200

    adding sliding animation to splash view

--- lib/Features/Splash/presntation/views/widgets/splash_view_body.dart
+++ lib/Features/Splash/presntation/views/widgets/splash_view_body.dart
@@ -1,9 +1,37 @@
+import 'package:bookly/Features/Splash/presntation/views/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../../core/utils/assets.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 10), end: Offset.zero)
+            .animate(animationController);
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,10 +43,7 @@ class SplashViewBody extends StatelessWidget {
         const SizedBox(
           height: 4,
         ),
-        const Text(
-          'Read Free Books',
-          textAlign: TextAlign.center,
-        )
+        SlidingText(slidingAnimation: slidingAnimation)
       ],
     );
   }
