cd12ac7367e7f7467184adf393ec673cf2aa90ac
vintx64 <vintx64@gmail.com>
2023-04-08 18:01:00 +0200

    add feature list view to home

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'custom_app_bar.dart';
-import 'custom_list_view_tem.dart';
+import 'featured_books_list_view.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -11,7 +11,7 @@ class HomeViewBody extends StatelessWidget {
     return Column(
       children: const [
         CustomAppBar(),
-        CustomListViewItem(),
+        FeaturedBooksListView(),
       ],
     );
   }
