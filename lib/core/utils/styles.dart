13ad16cfbf33f15c5bdecf660de2259a79776274
vintx64 <vintx64@gmail.com>
2023-04-10 01:37:56 +0200

    add font family

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 
 abstract class Styles {
-  static const titleMedium =
-      TextStyle(fontSize: 20, fontWeight: FontWeight.w600);
+  static const titleMedium = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'Montserrat',
+  );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,9 @@ class BooklyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: kPrimaryColor),
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: kPrimaryColor,
+      ),
       home: const SplashView(),
     );
   }
