80162ad762a50c1b90e271513edc53b82b4c29a4
vintx64 <vintx64@gmail.com>
2023-04-09 03:36:03 +0200

    create styles file and add title , home view

---
+++ lib/Features/home/presentation/views/widgets/featured_list_view_item.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../core/utils/assets.dart';
+
+class FeaturedListViewItem extends StatelessWidget {
+  const FeaturedListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 2.7 / 4,
+      child: Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            color: Colors.red,
+            image: const DecorationImage(
+                fit: BoxFit.fill,
+                image: AssetImage(
+                  AssetsData.test,
+                ))),
+      ),
+    );
+  }
+}
