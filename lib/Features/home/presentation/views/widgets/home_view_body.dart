cf99627adfc6d149ec4ceccb682ddf47a83cb279
vintx64 <vintx64@gmail.com>
2023-04-10 05:58:55 +0200

    modify fonts style

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../../core/utils/styles.dart';
@@ -13,7 +14,7 @@ class HomeViewBody extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 24),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: const [
+        children: [
           CustomAppBar(),
           FeaturedBooksListView(),
           SizedBox(
@@ -21,8 +22,40 @@ class HomeViewBody extends StatelessWidget {
           ),
           Text(
             'Best Seller',
-            style: Styles.titleMedium,
+            style: Styles.textStyly18.copyWith(fontFamily: 'Montserrat'),
           ),
+          BestSellerListViewItem(),
+        ],
+      ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  color: Colors.red,
+                  image: const DecorationImage(
+                      fit: BoxFit.fill,
+                      image: AssetImage(
+                        AssetsData.test,
+                      ))),
+            ),
+          ),
+          Column(
+            children: const [],
+          )
         ],
       ),
     );
--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -1,9 +1,15 @@
+import 'package:bookly/constants.dart';
 import 'package:flutter/material.dart';
 
 abstract class Styles {
-  static const titleMedium = TextStyle(
-    fontSize: 20,
-    fontWeight: FontWeight.w600,
-    fontFamily: 'Montserrat',
-  );
+  static const textStyly18 =
+      TextStyle(fontSize: 18, fontWeight: FontWeight.w600);
+  static const textStyly20 = TextStyle(
+      fontSize: 20, fontWeight: FontWeight.normal, fontFamily: kGtSectraFine);
+  static const textStyly30 = TextStyle(
+      fontSize: 30, fontWeight: FontWeight.normal, fontFamily: kGtSectraFine);
+  static const textStyly14 =
+      TextStyle(fontSize: 14, fontWeight: FontWeight.normal);
+  static const textStyly16 =
+      TextStyle(fontSize: 16, fontWeight: FontWeight.w500);
 }
