fb9504082a7ee4bfc0e3de8a96149fc55eaf780e
vintx64 <vintx64@gmail.com>
2023-04-11 22:31:54 +0200

    add title and subtitle to details view

--- lib/Features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:bookly/Features/home/presentation/views/widgets/custom_book_image.dart';
+import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
 import 'custom_book_details_app_bar.dart';
@@ -9,14 +10,34 @@ class BookDetailsViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
-    return Column(
-      children: [
-        SafeArea(child: CustomBookDetailsAppBar()),
-        Padding(
-          padding: EdgeInsets.symmetric(horizontal: width * .17),
-          child: CustomBookImage(),
-        ),
-      ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Column(
+        children: [
+          const SafeArea(child: CustomBookDetailsAppBar()),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: width * .2),
+            child: const CustomBookImage(),
+          ),
+          const SizedBox(
+            height: 43,
+          ),
+          Text(
+            'The Jungle Book',
+            style: Styles.textStyly30.copyWith(fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: 6,
+          ),
+          Opacity(
+            opacity: .7,
+            child: Text(
+              'Rudyard Kipling',
+              style: Styles.textStyly18.copyWith(fontStyle: FontStyle.italic),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
