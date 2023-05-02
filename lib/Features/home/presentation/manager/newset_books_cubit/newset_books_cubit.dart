eba213631b3ffb19d545fefde54cbc10d65d4b3c
vintx64 <vintx64@gmail.com>
2023-05-02 16:56:24 +0300

    newset books cubit done

--- lib/Features/home/presentation/manager/newset_books_cubit/newset_books_cubit.dart
+++ lib/Features/home/presentation/manager/newset_books_cubit/newset_books_cubit.dart
@@ -2,8 +2,24 @@ import 'package:bloc/bloc.dart';
 import 'package:bookly/Features/home/data/models/bookly_model/book_model.dart';
 import 'package:equatable/equatable.dart';
 
+import '../../../data/repos/home_repo.dart';
+
 part 'newset_books_state.dart';
 
 class NewsetBooksCubit extends Cubit<NewsetBooksState> {
-  NewsetBooksCubit() : super(NewsetBooksInitial());
+  NewsetBooksCubit(this.homeRepo) : super(NewsetBooksInitial());
+  final HomeRepo homeRepo;
+  Future<void> fetchNewsetBooks() async {
+    emit(NewsetBooksLoading());
+    var result = await homeRepo.fetchNewsetBooks();
+    result.fold((failure) {
+      emit(
+        NewsetBooksFailure(failure.errMessage),
+      );
+    }, (books) {
+      emit(
+        NewsetBooksSucess(books),
+      );
+    });
+  }
 }
