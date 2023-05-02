c1ffe7e37992e789849cecf3e237ea69334a6ace
vintx64 <vintx64@gmail.com>
2023-05-02 16:50:46 +0300

    create newest books cubit state

---
+++ lib/Features/home/presentation/manager/newset_books_cubit/newset_books_state.dart
@@ -0,0 +1,24 @@
+part of 'newset_books_cubit.dart';
+
+abstract class NewsetBooksState extends Equatable {
+  const NewsetBooksState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class NewsetBooksInitial extends NewsetBooksState {}
+
+class NewsetBooksLoading extends NewsetBooksState {}
+
+class NewsetBooksFailure extends NewsetBooksState {
+  final String errMessage;
+
+  const NewsetBooksFailure(this.errMessage);
+}
+
+class NewsetBooksSucess extends NewsetBooksState {
+  final List<BookModel> books;
+
+  NewsetBooksSucess(this.books);
+}
