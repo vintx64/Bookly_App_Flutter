d72070bc3a8d5e10d76e955436ed03f4d15017ec
vintx64 <vintx64@gmail.com>
2023-05-07 19:14:10 +0300

    fetch similar books

--- lib/Features/home/data/repos/home_repo.dart
+++ lib/Features/home/data/repos/home_repo.dart
@@ -5,4 +5,6 @@ import 'package:dartz/dartz.dart';
 abstract class HomeRepo {
   Future<Either<Failure, List<BookModel>>> fetchNewsetBooks();
   Future<Either<Failure, List<BookModel>>> featchFeaturedBooks();
+  Future<Either<Failure, List<BookModel>>> featchSimilarBooks(
+      {required String category});
 }
