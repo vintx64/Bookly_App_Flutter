665aaf3ca27898285c7061938a061fb84397d4c6
vintx64 <vintx64@gmail.com>
2023-05-16 04:31:31 +0300

    add shimmer to similar books

--- lib/Features/home/presentation/views/widgets/similar_books_list_view.dart
+++ lib/Features/home/presentation/views/widgets/similar_books_list_view.dart
@@ -1,4 +1,5 @@
 import 'package:bookly/Features/home/presentation/manager/similar_books_cubit/similar_books_cubit.dart';
+import 'package:bookly/Features/home/presentation/views/widgets/similar_books_shimmer.dart';
 import 'package:bookly/core/widgets/custom_error_widget.dart';
 import 'package:bookly/core/widgets/custom_loading_indicator.dart';
 import 'package:flutter/material.dart';
@@ -32,7 +33,7 @@ class SimilarListBooksView extends StatelessWidget {
         } else if (state is SimilarBooksFailure) {
           return CustomErrorWidget(errMessage: state.errMessage);
         } else {
-          return const CustomLoadingIndicator();
+          return const SimilarBooksShimmer();
         }
       },
     );
